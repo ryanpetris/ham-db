11cc00045b3c3b918f3ed2fce5213fd77fd9794a
Ryan Petris <ryan@petris.net>
2024-02-10 17:59:05 -0700

    MANIFEST.in needs to be in the root directory for wheel...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'hamdb'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESCRIPTION = 'Ham Database CLI and API'
 LONG_DESCRIPTION = 'Database of Amateur Radio Licensee Information'
 AUTHOR = 'Ryan Petris'
