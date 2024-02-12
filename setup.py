7253c974a006ffd323c0de1805d86aea4b9aa979
Ryan Petris <ryan@petris.net>
2024-02-12 05:06:36 -0700

    Make site name larger

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'hamdb'
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 DESCRIPTION = 'Ham Database CLI and API'
 LONG_DESCRIPTION = 'Database of Amateur Radio Licensee Information'
 AUTHOR = 'Ryan Petris'
